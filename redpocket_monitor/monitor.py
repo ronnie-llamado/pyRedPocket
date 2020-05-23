c1e403b48efad9342b4927c4bcaa7822db293c07
ronnie-llamado <llamado.ronnie@gmail.com>
2020-05-23 13:02:27 -0700

    save details_id to reduce future GETs

--- redpocket_monitor/monitor.py
+++ redpocket_monitor/monitor.py
@@ -20,26 +20,37 @@ def extract_csrf_from_html( html_text ):
 
 if __name__ == '__main__':
 
-    parser = configparser.ConfigParser()
-    parser.read( 'redpocket.ini' )
+    config = configparser.ConfigParser()
+    config.read( 'redpocket.ini' )
 
     client = requests.session()
 
     r = client.get( url=POST_URL )
 
     payload = {
-      'mdn' : parser[ 'redpocket.com' ][ 'username' ],
-      'password' : parser[ 'redpocket.com' ][ 'password' ],
+      'mdn' : config[ 'redpocket' ][ 'username' ],
+      'password' : config[ 'redpocket' ][ 'password' ],
       'csrf' : extract_csrf_from_html( r.text ),
     }
 
     r = client.post( url=POST_URL, data=payload )
 
-    r = client.get( url=GET_DETAILS_ID_URL )
-    params = json.loads( r.text )
-    details_id = params[ 'return_data' ][ 'confirmedLines' ][ 0 ][ 'hash' ]
+    try:
+        details_ids = json.loads( config[ 'redpocket' ][ 'details_ids' ] )
 
-    r = client.get( url=GET_PARAMS_URL % details_id )
-    params = json.loads( r.text )
-    data = params[ 'return_data' ]
-    pprint( data )
+    except KeyError:
+        r = client.get( url=GET_DETAILS_ID_URL )
+        details_params = json.loads( r.text )
+        details_ids = []
+        for iden in details_params[ 'return_data' ][ 'confirmedLines' ]:
+          details_ids.append( iden[ 'hash' ] )
+
+        config[ 'redpocket' ][ 'details_ids' ] = json.dumps( details_ids )
+        with open( 'redpocket.ini', 'w' ) as fil:
+            config.write( fil )
+
+    for iden in details_ids:
+      r = client.get( url=GET_PARAMS_URL % iden )
+      data_params = json.loads( r.text )
+      data = data_params[ 'return_data' ]
+      pprint( data )
